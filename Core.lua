8be5d0891ac0266af1557868f5efddea7a2c63a8
battlegroundsRBLX <thedavidbaszucki@gmail.com>
2026-02-14 02:23:28 -0600

    Update Core.lua

--- Core.lua
+++ Core.lua
@@ -2,7 +2,7 @@
 local Battlegrounds = {}
 Battlegrounds.Modules = {2,3}
 Battlegrounds.ModuleDetails = {                                      --version of the morphs // jf9s was here
-    [102643105617548]={Name="Morphs",Description="unblacklisted morphs",Version="1.0.0",Author="jf9s"}, 
+    [0]={Name="Morphs",Description="unblacklisted morphs",Version="1.0.0",Author="jf9s"}, 
     [0]={Name="Packages",Description="Manages loot drops and inventory",Version="0.9.5",Author="jf9s"},
     [0]={Name="ExtraModule",Description="Additional module example",Version="0.1.0",Author="jf9s"}
 }
